4ac2ee6e8434dd0e8a091437635b2d1f2b0d3246
Dev meena <btwdevxd@gmail.com>
2024-06-24 13:27:40 +0530

    Update start.py

--- Hack/plugins/start.py
+++ Hack/plugins/start.py
@@ -10,7 +10,7 @@ from telethon import events
 async def start(event):
     id = event.sender_id
     mention = f"[{event.sender.first_name}](tg://user?id={id})"
-    TEXT = "Hey {}, I am a Session Hacker Bot Supporting Both Pyrogram and Telethon Session String. Type /hack to see menu"
+    TEXT = "Hey {}, I am a Hacker Bot Supporting Both Pyrogram and Telethon Session String. Type /hack to see menu"
     await event.reply(TEXT.format(mention))
     if DB:
         await DB.add_user(id)
